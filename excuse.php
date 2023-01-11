b39ceae61199d2ae7eb477bb24871125af5d62f1
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-01-11 10:07:34 +0100

    Collect data from the form and display the appropriate excuse

--- excuse.php
+++ excuse.php
@@ -1,5 +1,27 @@
 <?php
+    $excuses = Array(
+        "illness" => "My -son/daughter-, will not be attending class today due health reasons. Our family doctor recommended that -name- took a day off.",
+        "death" => "Recently, we lost Felix, -name-'s cousin. The funeral is planned for tommorrow, for this reason our -son/daughter-, wont be attending school tommorrow.",
+        "extra" => "Tommorrow is the championship game of -name-'s football team. our -son/daugther- trained very hard for this event, therefore -name- wont be able to attend school tommorrow.",
+        "transportation" => "Due to some issues with the rail way, -name- will not be able to attend class.",
+    );
 
+
+    // Form processing
+    $excuse =  NULL;
+    $greeting = Null;
+    $closing_msg = NULL;
+    if (isset($_GET["name"]) and isset($_GET["teacher"]) and isset($_GET["gender"]) and isset($_GET["absence"])){
+        $child = $_GET["name"];
+        $gender = ($_GET["gender"] == "F" ? "daughter" : "son");
+        
+        $excuse = $excuses[$_GET["absence"]];
+        $excuse = str_replace("-name-", $child, $excuse);
+        $excuse = str_replace("-son/daughter-", $gender, $excuse);
+        
+        $greeting = "Dear, $_GET[teacher]";
+        $closing_msg = "Best regards, $child's parents";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +34,6 @@
 </head>
 <body>
     <main>
-
         <section>
             <h1>Excuse Notes Generator</h1>
             <p> Please fill-in the form below.</p>
@@ -45,13 +66,25 @@
                     <label for="extra">Significant extra-curricular activity</label>
                     <input type="radio" name="absence" id="extra" value="extra">
     
-                    <label for="other">Any other excuse of your choice</label>
-                    <input type="radio" name="absence" id="other" value="other">
+                    <label for="other">Public transportation</label>
+                    <input type="radio" name="absence" id="other" value="transportation">
                 </fieldset>
     
                 <input type="submit" name="submit" value="Submit">
             </form>
         </section>
+        <?php 
+            if ($excuse) {
+                echo "
+                        <div>
+                            <p> $greeting </p>
+                            <p> $excuse </p>
+                            <p> $closing_msg </p>
+                        </div>
+                     ";
+            }
+        
+        ?>
     </main>
 </body>
 </html>
