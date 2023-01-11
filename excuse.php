17beeeaf759f3bd1f8dcb182ae9907fa9b9922e4
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-01-11 10:34:26 +0100

    Add classes

--- excuse.php
+++ excuse.php
@@ -30,47 +30,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/main.css">
     <title>Excuse Notes Generator</title>
 </head>
 <body>
     <main>
-        <section>
-            <h1>Excuse Notes Generator</h1>
-            <p> Please fill-in the form below.</p>
-            <form method="get" action="./excuse.php">
-                <label for="name">Your child's name:</label>
-                <input type="text" name="name" id="name" required>
+        <header class="main-header">
+            <p class="main-header__logo">Excuse Notes Generator</p>
+        </header>
+        <section class="container">
+            <h1 class="main-heading"> Please fill-in the form below.</h1>
+
+            <form class="excuse-form" method="get" action="./excuse.php">
+
+                <label class="excuse-form__label" for="name">Your child's name:</label>
+                <input type="text" class="excuse-form__text-input" name="name" id="name" required>
     
-                <fieldset>
+                <fieldset class="excuse-form__fieldset">
                     <legend>Your child's gender</legend>
     
-                    <label for="f">F</label>
+                    <label class="excuse-form__label" for="f">F</label>
                     <input type="radio" name="gender" id="f" value="F" required>
     
-                    <label for="m">M</label>
+                    <label class="form-label" for="m">M</label>
                     <input type="radio" name="gender" id="m" value="M">
                 </fieldset>
     
-                <label for="teacher">The teacher's name:</label>
-                <input type="text" name="teacher" id="teacher" required>
+                <label class="excuse-form__label" for="teacher">The teacher's name:</label>
+                <input type="text" class="text-input" name="teacher" id="teacher" required>
     
-                <fieldset>
+                <fieldset class="excuse-form__fieldset">
                     <legend>Reason for the absence</legend>
     
-                    <label for="illness">Illnes</label>
+                    <label class="excuse-form__label" for="illness">Illnes</label>
                     <input type="radio" name="absence" id="illness" value="illness" required>
     
-                    <label for="death">Death of a pet (or a family member)</label>
+                    <label class="excuse-form__label" for="death">Death of a pet (or a family member)</label>
                     <input type="radio" name="absence" id="death" value="death">
     
-                    <label for="extra">Significant extra-curricular activity</label>
+                    <label class="excuse-form__label" for="extra">Significant extra-curricular activity</label>
                     <input type="radio" name="absence" id="extra" value="extra">
     
-                    <label for="other">Public transportation</label>
+                    <label class="excuse-form__label" for="other">Public transportation</label>
                     <input type="radio" name="absence" id="other" value="transportation">
                 </fieldset>
     
-                <input type="submit" name="submit" value="Submit">
+                <input type="submit" class="excuse-form__submit-btn" name="submit" value="Submit">
             </form>
         </section>
         <?php 
